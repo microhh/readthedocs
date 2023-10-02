3dba36f77aae1d79da5f5fce3b7cd610700c526d
Bart van Stratum <bart@vanstratum.com>
2023-10-02 12:25:45 +0200

    Revert "Update `.ini` parameters: A and B."
    This reverts commit f45d3c5b2528854aea424a08464c7c75c84017c7.

--- python/create_table.py
+++ python/create_table.py
@@ -60,7 +60,6 @@ class Table:
             print_hline('-')
         print('')
 
-
 """
 AAAAAAAAAAAAAAAAAAAAA
 """
@@ -69,20 +68,15 @@ advec.add_row('swadvec', 'swspatialorder', [
         'Advection scheme',
         '``0``: Disabled',
         '``2``: 2nd-order',
-        '``2i4``: 2nd-order with 4th-order interpolations',
-        '``2i5``: 2nd-order with 5th-order interpolations',
-        '``2i62``: 2nd-order with 6th(xy)/2nd(z)-order interpolation',
+        '``2i4``: 2nd-order (4th-order interpolation)',
+        '``2i4``: 2nd-order (4th-order interpolation)',
+        '``2i5``: 2nd-order (5th-order interpolation)',
+        '``2i53``: 2nd-order (5th/3rd-order interpolation)',
+        '``2i62``: 2nd-order (6th/2nd-order interpolation)',
         '``4``: 4th-order (DNS, high accuracy)',
         '``4m``: 2nd-order (DNS, energy conserving)'])
 advec.add_row('cflmax', '1.0', 'Max. CFL for adaptive time stepping')
-advec.add_row('fluxlimit_list', 'Empty list', 'Use flux limiter for scalars (2i5 and 2i62 only)')
 advec.print()
-
-aerosol = Table()
-aerosol.add_row('swaerosol', 'false', 'Switch for aerosols in radiation')
-aerosol.add_row('swtimedep', 'false', 'Switch for time dependent aerosols')
-aerosol.add_row('tdep_aermr{01-11}', '[aerosol] swtimedep', 'Aerosol individual time depenence switches')
-aerosol.print()
 
 """
 BBBBBBBBBBBBBBBBBBBBB
@@ -90,7 +84,7 @@ BBBBBBBBBBBBBBBBBBBBB
 boundary = Table()
 boundary.add_row('swboundary', 'None', [
         'Boundary discretization',
-        '``default``: Resolved boundaries',
+        '``default``: resolved boundaries',
         '``surface``: MOST-based surface model',
         '``surface_lsm``: MOST-based surface model with HTESSEL LSM',
         '``surface_bulk``: Surface model with prescribed drag coefficients'])
@@ -127,52 +121,15 @@ boundary.add_row('stop', 'None', [
         'Values can be specified per scalar: ``stop[qt]=0``.'])
 boundary.add_row('sbot_2d_list', 'Empty list', [
         'Comma-separate list of scalars that provide a binary',
-        'file (``sbot_thl_in.0000000``) with 2D slice'])
+        'file (``sbot_thl.0000000``) with 2D slice'])
+boundary.add_row('swtimedep_sbot_2d', '0', 'Enable time varying 2D surface fields')
+boundary.add_row('sbot_2d_loadtime', 'None', 'Frequency of 2D surface input')
 boundary.add_row('z0m', 'None', 'Roughness length of momentum (m)')
 boundary.add_row('z0h', 'None', 'Roughness length of heat (m)')
-boundary.add_row('swconstantz0', 'true', [
-        'Switch for spatially homogeneous z0m/z0h',
-        '``true``: Homogeneous z0m/z0h, from ``.ini`` file',
-        '``false``: Heterogeneous z0m/z0h from ``z0m.0000000``/``z0h.0000000``'])
-boundary.add_row('swcharnock', 'false', 'Switch for Charnock parameterization (``boundary_surface`` only)')
-boundary.add_row('alpha_m', 'None', 'Parameter Charnock parameterization')
-boundary.add_row('alpha_ch', 'None', 'Parameter Charnock parameterization')
-boundary.add_row('alpha_h', 'None', 'Parameter Charnock parameterization')
 boundary.add_row('ustar', 'None', 'Value of the fixed friction velocity (m s-1)')
 boundary.add_row('bulk_cm', 'None', 'Drag coefficient for momentum (-)')
 boundary.add_row('bulk_cs', 'None', 'Drag coefficient for scalar (-)')
-boundary.add_row('swtimedep', 'false', 'Switch for time varying surface BCs')
-boundary.add_row('timedeplist', 'Empty list', 'List of scalars with time varying BCs')
-boundary.add_row('swtimedep_sbot_2d', 'false', 'Switch for time varying 2D surface BCs')
-boundary.add_row('sbot_2d_loadtime', 'None', 'Frequency of 2D surface BC input')
-boundary.add_row('scalar_outflow', 'Empty list', 'List of scalars with non-periodic lateral BCs')
-boundary.add_row('flow_direction', 'None', [
-        'Flow direction used for ``scalar_outflow`` at each lateral edge',
-        '``inflow``: Inflow (Dirichlet BC)',
-        '``outflow``: Outflow (Neumann BC)'])
-boundary.add_row('swtimedep_outflow', 'false', 'Switch for time varying scalar outflow')
 boundary.print()
-
-lsm1 = Table()
-lsm1.add_row('swhomogeneous', 'true', 'Use spatially homogeneous land-surface properties')
-lsm1.add_row('swfreedrainage', 'true', 'Free drainage BC at bottom of soil column')
-lsm1.add_row('swwater', 'false', 'Switch for allowing open water')
-lsm1.add_row('swtilestats', 'false', 'Output individual tile statistics')
-lsm1.add_row('swtilestats_column', 'false', 'Output individual tile column statistics')
-lsm1.add_row('emis_sfc', 'None', 'Surface emissivity')
-lsm1.print()
-
-lsm2 = Table()
-lsm2.add_row('gD', 'None', 'gD coefficient in VDP reduction canopy resistance (?)')
-lsm2.add_row('c_veg', 'None', 'Sub-grid vegetation fraction (0-1)')
-lsm2.add_row('lai', 'None', 'Leaf area index (m2 m-2)')
-lsm2.add_row('rs_veg_min', 'None', 'Minium canopy resistance (s m-1)')
-lsm2.add_row('rs_soil_min', 'None', 'Minium soil resistance (s m-1)')
-lsm2.add_row('lambda_stable', 'None', 'Skin conductivity stable conditions (W m-2 K-1)')
-lsm2.add_row('lambda_unstable', 'None', 'Skin conductivity unstable conditions (W m-2 K-1)')
-lsm2.add_row('cs_veg', 'None', 'Heat capacity skin layer (J K-1)')
-lsm2.print()
-
 
 budget = Table()
 budget.add_row('swbudget', '0', [
@@ -182,34 +139,40 @@ budget.add_row('swbudget', '0', [
 budget.print()
 
 buffer = Table()
-buffer.add_row('swbuffer', 'false', 'Switch for the buffer layer')
-buffer.add_row('swupdate', 'false', 'Switch whether to update the buffer with actual mean profiles')
+buffer.add_row('swbuffer', '0', [
+        'Switch for the buffer layer',
+        '``0``: Buffer layer disabled',
+        '``1``: Buffer layer enabled'])
+buffer.add_row('swupdate', '0', [
+        'Switch whether to update the buffer with actual mean profiles',
+        '``0``: Updating disabled',
+        '``1``: Updating enabled'])
 buffer.add_row('zstart', 'None', 'Height in domain at which the buffer layer starts (m)')
 buffer.add_row('sigma', 'None', 'Damping frequency of buffer layer (rad s-1)')
 buffer.add_row('beta', '2', 'Exponent of strength reduction function (-)')
 buffer.print()
 
-#"""
-#CCCCCCCCCCCCCCCCCCCCC
-#"""
-#cross1 = Table()
-#cross1.add_row('swcross', '0', 'Switch for cross sections')
-#cross1.add_row('sampletime', 'None', 'Time between consecutive samples (s)')
-#cross1.add_row('crosslist', 'None', 'List of cross sections to be made')
-#cross1.add_row('xy', 'None', 'List of z-levels for xy-cross sections')
-#cross1.add_row('xz', 'None', 'List of y-levels for xz-cross sections')
-#cross1.add_row('yz', 'None', 'List of x-levels for yz-cross sections')
-#cross1.print()
-#
-## TO-DO: create option for two column tables.
-#cross2 = Table()
-#cross2.add_row('*', '', 'Any prognostic or diagnostic variable')
-#cross2.add_row('*path', '', 'Density-weighted vertical integral of any prognostic or diagnostic variable')
-#cross2.add_row('*bot', '', 'Bottom boundary value of any prognostic variable')
-#cross2.add_row('*top', '', 'Top boundary value of any prognostic variable')
-#cross2.add_row('*fluxbot', '', 'Bottom boundary flux of any prognostic variable')
-#cross2.add_row('*fluxtop', '', 'Top boundary flux of any prognostic variable')
-#cross2.add_row('*lngrad', '', 'Logarithm of the length of the gradient vector for any prognostic variable')
-#cross2.print()
-#
-#
+"""
+CCCCCCCCCCCCCCCCCCCCC
+"""
+cross1 = Table()
+cross1.add_row('swcross', '0', 'Switch for cross sections')
+cross1.add_row('sampletime', 'None', 'Time between consecutive samples (s)')
+cross1.add_row('crosslist', 'None', 'List of cross sections to be made')
+cross1.add_row('xy', 'None', 'List of z-levels for xy-cross sections')
+cross1.add_row('xz', 'None', 'List of y-levels for xz-cross sections')
+cross1.add_row('yz', 'None', 'List of x-levels for yz-cross sections')
+cross1.print()
+
+# TO-DO: create option for two column tables.
+cross2 = Table()
+cross2.add_row('*', '', 'Any prognostic or diagnostic variable')
+cross2.add_row('*path', '', 'Density-weighted vertical integral of any prognostic or diagnostic variable')
+cross2.add_row('*bot', '', 'Bottom boundary value of any prognostic variable')
+cross2.add_row('*top', '', 'Top boundary value of any prognostic variable')
+cross2.add_row('*fluxbot', '', 'Bottom boundary flux of any prognostic variable')
+cross2.add_row('*fluxtop', '', 'Top boundary flux of any prognostic variable')
+cross2.add_row('*lngrad', '', 'Logarithm of the length of the gradient vector for any prognostic variable')
+cross2.print()
+
+
